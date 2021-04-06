50ecb519f4c05b3b6b4d9ef60d9f455af143e65a
chc72331 <chc72331@creighton.edu>
2021-04-05 23:07:35 -0500

    Update deleteCustomer.js

--- forms/deleteCustomer/deleteCustomer.js
+++ forms/deleteCustomer/deleteCustomer.js
@@ -1,10 +1,10 @@
-btnCustomer.onclick = function() {
+deleteCustomer.onshow = function() {
   query = "SELECT name FROM customers"
   req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=" + netID + "&pass=" + pw + "&database=" + netID + "&query=" + query)
   if (req.status == 200) {
     results = JSON.parse(req.responseText)
     if (results.length == 0)
-      lblMessage.value = "There are no customers in the database."
+      lblCustomer.value = "There are no customers in the database."
     else {
       for (i = 0; i < results.length; i++)
         selCustomer.addItem(results[i][0])
@@ -13,11 +13,13 @@ btnCustomer.onclick = function() {
 }
 
 
+
 btnDelCustomer.onclick = function() {
   let employeeDelete = ""
-  employeeDelete = selCustomers.value
+  employeeDelete = selCustomer.value
   query = "DELETE FROM customers WHERE  name = '" + employeeDelete + "'"
   req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=" + netID + "&pass=" + pw + "&database=" + netID + "&query=" + query)
+  lblCustomer.value = "You have successfully deleted a customer!"
 }
 
 btnRefresh.onclick = function() {
