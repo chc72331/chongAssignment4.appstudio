fe289a05a89a9e507cc2e62fdbe0387532a6f3d0
chc72331 <chc72331@creighton.edu>
2021-04-05 22:56:19 -0500

    Create deleteCustomer.js

---
+++ forms/deleteCustomer/deleteCustomer.js
@@ -0,0 +1,25 @@
+btnCustomer.onclick = function() {
+  query = "SELECT name FROM customers"
+  req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=" + netID + "&pass=" + pw + "&database=" + netID + "&query=" + query)
+  if (req.status == 200) {
+    results = JSON.parse(req.responseText)
+    if (results.length == 0)
+      lblMessage.value = "There are no customers in the database."
+    else {
+      for (i = 0; i < results.length; i++)
+        selCustomer.addItem(results[i][0])
+    }
+  }
+}
+
+
+btnDelCustomer.onclick = function() {
+  let employeeDelete = ""
+  employeeDelete = selCustomers.value
+  query = "DELETE FROM customers WHERE  name = '" + employeeDelete + "'"
+  req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=" + netID + "&pass=" + pw + "&database=" + netID + "&query=" + query)
+}
+
+btnRefresh.onclick = function() {
+  location.reload()
+}
